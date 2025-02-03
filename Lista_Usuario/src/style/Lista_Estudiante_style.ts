0b59b4444d3e403a9f03c49811fe8e750d9d0266
PaoDerass <paola_deras@unitec.edu>
2025-02-02 21:15:48 -0600

    estilos

---
+++ Lista_Usuario/src/style/Lista_Estudiante_style.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  contenedor: {
+    flex: 1,
+    padding: 20,
+    alignItems: "center",
+    backgroundColor: "#E6E6FA", 
+  },
+  titulo: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+    color: "#6A0DAD", 
+  },
+  estudiante: {
+    fontSize: 18,
+    padding: 5,
+    color: "#4B0082",
+  },
+});
